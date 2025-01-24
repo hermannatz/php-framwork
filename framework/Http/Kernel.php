c6bb5015b0d29f2ec95bea705aa1dbd2fe9a9ad6
hermann.azong <hermann.azong@timewaver.com>
2025-01-24 20:24:25 +0100

    Code refactoring Kernel handle method

--- framework/Http/Kernel.php
+++ framework/Http/Kernel.php
@@ -2,13 +2,26 @@
 
 namespace CodeBrain\Framework\Http;
 
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
 	public function handle(Request $request): Response
 	{
 
-		$content = '<h1>Hello Response!</h1>';
-		return new Response($content);
+		$dispatcher = simpleDispatcher(function( RouteCollector $routeCollector){
+			$routeCollector->addRoute('GET', '/', function () {
+				$content = '<h1>Hello Response!</h1>';
+				return new Response($content);
+			});
+		});
+
+		$routeInfo = $dispatcher->dispatch($request->server['REQUEST_METHOD'], $request->server['REQUEST_URI']);
+
+		[$status, $handler, $vars] = $routeInfo;
+		return $handler($vars);
+
 	}
 
 }

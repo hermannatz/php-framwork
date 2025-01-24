a03eb7e8d32393d2b07851a125571e9b784c692f
hermann.azong <hermann.azong@timewaver.com>
2025-01-24 20:02:37 +0100

    Add package for php fast router

--- public/index.php
+++ public/index.php
@@ -1,23 +1,18 @@
 <?php declare(strict_types=1);
 
+use CodeBrain\Framework\Http\Kernel;
 use CodeBrain\Framework\Http\Request;
 use CodeBrain\Framework\Http\Response;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-# request received
-
+# Request received
 $request = Request::createFromGlobals();
-# perform some business logic
 
-# send response (json)
-
-# $content = '<h1>Hello World</h1>';
-
-# $response = new Response(content: $content, status: 200, headers: []);
-
+# Perform business logic
 $kernel = new Kernel();
 
+# Send response
 $response = $kernel->handle($request);
 
 $response->send();
